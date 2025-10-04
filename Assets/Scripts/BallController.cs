a5f702f9651517120da8b0ee7e2b2c0c2d1c5351
zahra <habiballahi>
2025-10-05 00:28:59 +0330

    Add GameOver System

--- Assets/Scripts/BallController.cs
+++ Assets/Scripts/BallController.cs
@@ -1,28 +1,44 @@
 using System.Collections;
 using System.Collections.Generic;
+using Unity.VisualScripting;
 using UnityEngine;
 
 public class BallController : MonoBehaviour
 {
+    public LogicController Logic;
     [SerializeField] private Rigidbody2D rigidbody;
+
     [SerializeField] private float JumpSpeed;
+
+     public bool ActiveBall=true; 
     // Start is called before the first frame update
     void Start()
     {
-
+        Logic = GameObject.FindGameObjectWithTag("Logic").GetComponent<LogicController>();
     }
 
     // Update is called once per frame
     void Update()
     {
-        if (Input.GetKeyDown(KeyCode.Space) == true)
+        if (Input.GetKeyDown(KeyCode.Space) == true && ActiveBall == true)
         {
             Jumping();
+        }
+
+        if (transform.position.y > 2 || transform.position.y < -2)
+        { 
+             Logic.GameOver();
         }
     }
 
     void Jumping()
     {
         rigidbody.velocity = Vector2.up * JumpSpeed;
+    }
+
+    void OnCollisionEnter2D(Collision2D collision)
+    {
+        Logic.GameOver();
+       
     }
 }
--- Assets/Scripts/LogicController.cs
+++ Assets/Scripts/LogicController.cs
@@ -2,17 +2,38 @@ using System.Collections;
 using System.Collections.Generic;
 using TMPro;
 using UnityEngine;
+using UnityEngine.SceneManagement;
 using UnityEngine.UI;
 
 public class LogicController : MonoBehaviour
 {
+    [SerializeField] private GameObject GameOverScreen;
     [SerializeField] public int PlayerScore;
-    [SerializeField] public TextMeshProUGUI  ScoreText;
+    [SerializeField] public TextMeshProUGUI ScoreText;
 
+    private BallController ballController;
+    void Start()
+    {
+        ballController = GameObject.FindAnyObjectByType<BallController>();
+    }
     public void AddScore()
     {
         PlayerScore = PlayerScore + 1;
         ScoreText.text = PlayerScore.ToString();
         Debug.Log("AddScore");
     }
+
+    public void ResetGame()
+    {
+
+        SceneManager.LoadScene(SceneManager.GetActiveScene().name);
+    }
+
+    public void GameOver()
+    {
+        GameOverScreen.SetActive(true);
+        ballController.ActiveBall = false;
+    }
+
+
 }
